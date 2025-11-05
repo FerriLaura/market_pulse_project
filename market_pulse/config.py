e123837cd6e970538c7d1eab0fa2dfc34f49625c
laura <laura.ferri5@studenti.unimi.it>
2025-11-05 20:26:22 +0100

    Update config.py
    Sector ETFs instead of using the single tickers representatives of the sectors.

--- market_pulse/config.py
+++ market_pulse/config.py
@@ -1,13 +1,14 @@
-# Defining a dictionary with the sectors and the thickers I want to include in the analysis: TICKERS_BY_SECTOR
+# --- Sector ETFs ------
 
-TICKERS_BY_SECTOR = {
-    "Technology": ["AAPL", "MSFT", "NVDA"],
-    "Energy": ["XOM", "CVX", "COP"],
-    "Healthcare": ["JNJ", "PFE", "ABBV"],
+SECTOR_ETFS = {
+    "Technology": "XLK",
+    "Energy": "XLE",
+    "Healthcare": "XLV",
 }
 
 # Default configuration values
-DEFAULT_PERIOD = "5y" 
+DEFAULT_PERIOD = "1y" 
 DEFAULT_INTERVAL = "1d"
 AUTO_ADJUST = True 
 TRADING_DAYS_PER_YEAR = 252
+RISK_FREE_ANNUAL = 0.02 # 2% 
